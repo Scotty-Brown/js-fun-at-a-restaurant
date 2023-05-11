7cd5fe919055fdf9c4e6daf98ff2c759f723376d
Scotty Brown <scottybrown98@gmail.com>
2023-05-10 20:42:49 -0700

    Pass and unskip first 3 meal test

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,29 @@
-function nameMenuItem() {
 
+// nameMenueItems
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
+
+// createMenuItem
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type,
+  }
+}
+
+// addIngredients
+function addIngredients(item1, ingredients) {
+ ingredients.push(item1)
+}
+// Working notes // - I didnt include return because we are only needing to put the item in the array.
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
