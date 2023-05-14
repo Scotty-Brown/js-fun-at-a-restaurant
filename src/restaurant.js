42cde332824f65774ae3fc3f1430a418b1e11a84
Scotty Brown <scottybrown98@gmail.com>
2023-05-14 11:26:17 -0700

    Complete all restaurant.js functions

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,8 +1,52 @@
+// createRestaurant
 
+  function createRestaurant (restName) {
+    return {
+      name: restName,
+      menus: {
+        breakfast: [],
+        lunch: [],
+        dinner: []
+      }
+    }
+}
+
+// addMenuItem
+
+function addMenuItem(restName, item) {
+ if (item.type === 'breakfast' && !restName.menus.breakfast.includes(item) ){
+  restName.menus.breakfast.push(item)
+ } else if (item.type === "lunch" && !restName.menus.lunch.includes(item)){
+  restName.menus.lunch.push(item)
+ } else if (item.type === "dinner" && !restName.menus.dinner.includes(item)) {
+  restName.menus.dinner.push(item)
+ } 
+}
+
+// removeMenuItem
+
+  function removeMenuItem(restName, itemtoRemove, type) {
+    for (var i = 0; i < restName.menus[type].length; i++) {
+      if (restName.menus[type][i].name === itemtoRemove) {
+        restName.menus[type].splice(i, 1)
+       return `No one is eating our ${itemtoRemove} - it has been removed from the ${type} menu!`
+      }
+    } 
+    return `Sorry, we don't sell ${itemtoRemove}, try adding a new recipe!`
+}
+
+//checkForFood
+function checkForFood(restName, foodItem) {
+  for (var i = 0; i < restName.menus[foodItem.type].length; i++) {
+    if (restName.menus[foodItem.type][i].name === foodItem.name) {
+      return `Yes, we're serving ${foodItem.name} today!`;
+    }
+  } return `Sorry, we aren't serving ${foodItem.name} today.`
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem,
-  // checkForFood
+  createRestaurant, 
+  addMenuItem,
+  removeMenuItem,
+  checkForFood
 }
